d997086ef163204dc84be2743bd123280c0b05a4
Pedro Gaya <pedrohenriquegaya@gmail.com>
2020-11-05 14:50:28 -0300

    auto music loop

--- commands/music.js
+++ commands/music.js
@@ -17,6 +17,7 @@ module.exports = {
       });
 
       dispatcher.on("finish", () => {
+        play(connection, args[0])
         console.log(`Stopped music on ${message.guild.name}`);
       });
 
