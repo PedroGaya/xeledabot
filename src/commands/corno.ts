117c07182d4934761c2cbc3b5f984b3a2e0d819d
Pedro Gaya <pedrohenriquegaya@gmail.com>
2020-12-31 22:05:35 -0300

    corno refactor

--- src/commands/corno.ts
+++ src/commands/corno.ts
@@ -1,19 +1,26 @@
-module.exports = {
-	name: 'corno',
-    description: 'Made to test the bots\' basic functions',
-    args: false,
-	async execute(message, args) {
-        var author = message.author;
+import Discord = require("discord.js");
+import { Command } from "../core/types";
 
-        if (author.username !== 'Wander') {message.channel.send(`${author} ${getCorno()}`);}
-        else {message.channel.send(`${author} não é corno`);}
+export const corno: Command = {
+	name: "corno",
+	description: "Made to test the bot's basic functions",
+	args: false,
+	execute: async (message: Discord.Message, _args: string[]) => {
+		var author = message.author;
+
+		if (author.username !== "Wander") {
+			message.channel.send(`${author} ${getCorno()}`);
+		} else {
+			message.channel.send(`${author} não é corno`);
+		}
 	},
 };
 
 function getCorno() {
-    var random = Math.random() * 2
-    if (Math.ceil(random) == 2) {
-        return 'é corno.'
-    }
-    else {return 'não é corno.'}
+	var random = Math.random() * 2;
+	if (Math.ceil(random) == 2) {
+		return "é corno.";
+	} else {
+		return "não é corno.";
+	}
 }
