3b2b2202a132533030815ddae200d534cc53bf18
Pedro Gaya <pedrohenriquegaya@gmail.com>
2020-12-31 23:42:43 -0300

    index refactor

--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,62 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+	require("dotenv").config();
 }
 
-const fs = require("fs");
-const Discord = require("discord.js");
-const config = require("../config.json");
-const helper = require("./core/helper.js");
+import fs = require("fs");
+import Discord = require("discord.js");
+import { Client, Command } from "./core/types";
 
-const prefix = config.prefix;
+const prefix = "x/";
 
-const client = new Discord.Client();
+const client = new Discord.Client() as Client;
 client.commands = new Discord.Collection();
 
 const commandFiles = fs
-  .readdirSync("./src/commands")
-  .filter((file) => file.endsWith(".js"));
+	.readdirSync("./src/commands")
+	.filter((file) => file.endsWith(".ts"));
 
 for (const file of commandFiles) {
-  const command = require(`./commands/${file}`);
-  client.commands.set(command.name, command);
+	const command: Command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
 }
 
 client.once("ready", async () => {
-  console.log("XeledaBot is ready.");
-  console.log(`Process started in ${process.env.NODE_ENV} enviroment.`)
+	console.log("XeledaBot is ready.");
+	console.log(`Process started in ${process.env.NODE_ENV} enviroment.`);
 });
 
-client.on("message", async (message) => {
-  if (!message.content.startsWith(prefix) || message.author.bot) return;
+client.on("message", async (message: Discord.Message) => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-  const args = message.content.slice(prefix.length).split(/ +/);
-  const commandName = args.shift().toLowerCase();
+	const args = message.content.slice(prefix.length).split(/ +/);
 
-  const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
-  if (!command) return;
+	const commandName = (args.shift() ?? "help").toLowerCase();
 
-  if (command.args && !args.length) {
-    let reply = `${message.author} Não foram providenciados os parâmetros necessários.`;
+	const command =
+		client.commands.get(commandName) ||
+		client.commands.find(
+			(cmd: any) => cmd.aliases && cmd.aliases.includes(commandName)
+		);
+	if (!command) return;
 
-    if (command.usage) {
-        reply += `\n${message.author} Uso correto: \`${prefix}${command.name} ${command.usage}\``;
-    }
+	if (command.args && !args.length) {
+		let reply = `${message.author} Não foram providenciados os parâmetros necessários.`;
 
-    return message.channel.send(reply);
-  }
+		if (command.usage) {
+			reply += `\n${message.author} Uso correto: \`${prefix}${command.name} ${command.usage}\``;
+		}
 
-  try {
-    command.execute(message, args);
-  } catch (error) {
-    console.error(error);
-    message.channel.send(`${message.author} ocorreu um erro na execução desse comando.`);
-  }
+		return message.channel.send(reply);
+	}
+
+	try {
+		command.execute(message, args);
+	} catch (error) {
+		console.error(error);
+		message.channel.send(
+			`${message.author} ocorreu um erro na execução desse comando.`
+		);
+	}
 });
 
 client.login(process.env.BOT_TOKEN);
